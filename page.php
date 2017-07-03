b87a15c5837745e40348daa63eee41a66294430a
hp-works <info@hp-work.net>
2017-07-03 14:04:40 +0900

    固定ページに対応していなかった為、固定ページ対応

--- page.php
+++ page.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: developer
- * Date: 2017/02/24
- * Time: 11:41
- */
+
+get_header();
+
+if(have_posts()):
+    while(have_posts()):
+        the_post();
+        /**
+         * 拡張Postクラスを作成
+         * @var $extends ero\model\extendPost
+         */
+        $extends = ero\Controller::getExtendPost($post);
+        ?>
+        <div class="container">
+
+            <article id="post-<?php the_ID(); ?>" class="col-xs-1 main-center detail <?php post_class(); ?>">
+
+                <h2><?php print the_title(); ?></h2>
+
+                <div class="movie-info message">
+                    <?php
+                    the_content();
+                    wp_link_pages();
+                    next_posts_link();
+                    previous_posts_link();
+                    ?>
+                </div>
+
+                <div class="clearfix"></div>
+
+            </article>
+
+            <?php get_sidebar(); ?>
+
+        </div>
+
+        <?php
+    endwhile;
+endif;
+
+get_footer();
